778588e75bd837a5ae9f29ad8a4af23ea96b28a8
G284162022 <228416@st.takushoku-u.ac.jp>
2023-06-01 15:50:12 +0900

    jghffds

--- practice-html/gourmet.js
+++ practice-html/gourmet.js
@@ -207,7 +207,7 @@ b.addEventListener('click', sendRequest);
 
 function sendRequest() {
   // URL を設定
-  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json';
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/G006.josn';
 
 axios.get(url)
 .then(showResult)   
