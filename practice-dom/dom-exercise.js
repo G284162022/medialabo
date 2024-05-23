568839a507d4eb3eec06aa8d55dc26f903c08835
G284162022 <228416@st.takushoku-u.ac.jp>
2024-05-23 16:09:51 +0900

    sdjbviwds

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -33,7 +33,7 @@ for (let student of students) {
 
 // 練習4-4 箇条書き削除プログラム
 
-let w=document.querySelectorAll('ul#location > li');
-w.remove();
+let w = document.querySelector('ul#location');
+w.remove();										
 
 // 練習4-5 箇条書き追加プログラム
